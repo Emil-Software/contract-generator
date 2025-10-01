b69fdf144f8feb6843d786feb59826883b92960a
Pietro <59828604+pg-88@users.noreply.github.com>
2025-10-01 12:13:48 +0200

    port 3005

--- backend/main.ts
+++ backend/main.ts
@@ -16,7 +16,7 @@ async function bootstrap(): Promise<void> {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  const port = process.env.PORT ? Number(process.env.PORT) : 3005;
   await app.listen(port);
   // eslint-disable-next-line no-console
   console.log(`TemplateGenerator API listening on http://localhost:${port}/api`);
